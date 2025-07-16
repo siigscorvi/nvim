78df365c85f998c1f56a9fb0705dbe0b2fa82862
siigscorvi <s76rhart@uni-bonn.de>
2025-07-16 15:36:10 +0200

    refactor: switch to treesitter with all grammars

--- plugins.nix
+++ plugins.nix
@@ -1,127 +1,78 @@
 { pkgs }:
 # List all the plugins I need here
-let
-  treesitter = p: with p; [
-      asm
-      bash
-      bibtex
-      c
-      cmake
-      comment
-      cpp
-      css
-      csv
-      diff
-      dockerfile
-      doxygen
-      git_config
-      git_rebase
-      gitattributes
-      gitcommit
-      gitignore
-      go
-      gomod
-      gotmpl
-      gosum
-      gowork
-      html
-      http
-      json
-      latex
-      lua
-      luadoc
-      luap
-      make
-      markdown
-      markdown_inline
-      nix
-      printf
-      python
-      regex
-      requirements
-      sql
-      ssh_config
-      tmux
-      toml
-      vim
-      vimdoc
-      xml
-      yaml
-    ];
-in
-  with pkgs.vimPlugins; [
+with pkgs.vimPlugins; [
 # debugging
 
 # editing support
-    blink-cmp
-    blink-emoji-nvim
-    blink-nerdfont-nvim
-    blink-cmp-conventional-commits
+  blink-cmp
+  blink-emoji-nvim
+  blink-nerdfont-nvim
+  blink-cmp-conventional-commits
 
-    mini-ai
-    mini-pairs
-    mini-splitjoin
-    mini-surround
+  mini-ai
+  mini-pairs
+  mini-splitjoin
+  mini-surround
 
-    undotree
+  undotree
 
-    lazygit-nvim
-    gitsigns-nvim
+  lazygit-nvim
+  gitsigns-nvim
 
-    # copilot-lua
-    # blink-copilot
+  # copilot-lua
+  # blink-copilot
 
-    trouble-nvim
-    todo-comments-nvim
+  trouble-nvim
+  todo-comments-nvim
 
 # code analysis
-    nvim-lspconfig
-    (nvim-treesitter.withPlugins treesitter)
+  nvim-lspconfig
+  nvim-treesitter.withAllGrammars
 
-    ltex_extra-nvim
+  ltex_extra-nvim
 
 
 # navigation
-    plenary-nvim # dependency for telescope
-    telescope-nvim
-    telescope-fzf-native-nvim # fuzzy finder implementation for telescope
-    telescope-file-browser-nvim
-    # telescope-lua-snip-nvim # waiting for luasnip
-    mini-bufremove
-    harpoon2
+  plenary-nvim # dependency for telescope
+  telescope-nvim
+  telescope-fzf-native-nvim # fuzzy finder implementation for telescope
+  telescope-file-browser-nvim
+  # telescope-lua-snip-nvim # waiting for luasnip
+  mini-bufremove
+  harpoon2
 
-    tmux-nvim
+  tmux-nvim
 
 
 # styling
-    gruvbox-nvim
+  gruvbox-nvim
 
-    nvim-web-devicons
-    lspkind-nvim
+  nvim-web-devicons
+  lspkind-nvim
 
-    which-key-nvim
+  which-key-nvim
 
-    nui-nvim
-    nvim-notify
-    noice-nvim
+  nui-nvim
+  nvim-notify
+  noice-nvim
 
-    mini-cursorword
-    mini-indentscope
-    mini-trailspace
+  mini-cursorword
+  mini-indentscope
+  mini-trailspace
 
-    lualine-nvim
-    # copilot-lualine
+  lualine-nvim
+  # copilot-lualine
 
-    promise-async
-    nvim-ufo
+  promise-async
+  nvim-ufo
 
-    nvim-colorizer-lua
+  nvim-colorizer-lua
 # misc
-    snacks-nvim
+  snacks-nvim
 
 
 # latex
-    vimtex
+  vimtex
 
 # markdown
   render-markdown-nvim
